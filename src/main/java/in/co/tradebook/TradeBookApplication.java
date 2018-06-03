9e4ee24a67dc4111b2ff1f934f10528a82e4234b
Shahroz Saleem <shahroz.saleem@dhdigital.co.in>
2018-06-03 22:03:04 +0530

    Initialize project

---
+++ src/main/java/in/co/tradebook/TradeBookApplication.java
@@ -0,0 +1,13 @@
+package in.co.tradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBookApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TradeBookApplication.class, args);
+	}
+
+}
